06158dd7dc6f4d886a21e456ad3ac6741da6049f
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2026-08-05 18:30:12 +0530

    Create PrimeNumber.java

---
+++ PrimeNumber.java
@@ -0,0 +1,13 @@
+class Solution {
+    static boolean isPrime(int n) {
+        if(n<2 || n%2==0)
+            return false;
+        if(n==2)
+            return true;
+        for(int i=3; i*i<=n; i+=2){
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+}
