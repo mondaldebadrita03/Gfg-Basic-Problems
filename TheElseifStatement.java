d30621611c3f4181091572532d02e5d863c54aba
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2026-08-05 18:42:29 +0530

    Create TheElseifStatement.java

---
+++ TheElseifStatement.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static void utility(int number) {
+        if(number>100){
+            System.out.print("Big");
+        }else if(number<10){
+            System.out.print("Small");
+        }else{
+            System.out.print("Number");
+        }
+    }
+}
